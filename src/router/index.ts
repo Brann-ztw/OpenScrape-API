1ad09033652a54fa1c78db4cfb8f20043c31a591
BranZtw <brandonggonzalezz@gmail.com>
2024-08-09 21:05:30 -0600

    front

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,15 @@
-import express, { Application} from 'express';
+import express, { Application, Request, Response } from 'express';
 import { routerDownloadYoutube } from './yotubeRouter';
 import { routerDownloadInstagram } from './intagramRouter'
+import path from 'path';
 export const app: Application = express();
  
 app.use('/download/yt', routerDownloadYoutube);
 app.use('/download/ig', routerDownloadInstagram);
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/', (req: Request, res: Response) => {
+  return res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+})
 
 
